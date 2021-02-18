ffefc2779dc53838a43a440b2e072f1248f76fb6
ertugrulcetin <ertu.ctn@gmail.com>
2021-02-18 19:49:42 +0300

    init backend

---
+++ backend-3d-scene/test/backend_3d_scene/core_test.clj
@@ -0,0 +1,8 @@
+(ns backend-3d-scene.core-test
+  (:require
+   [backend-3d-scene.core :refer :all]
+   [clojure.test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
