33af966512e11f4fdda891f1f2eee3e20e250c0c
ertugrulcetin <ertu.ctn@gmail.com>
2021-02-19 19:36:15 +0300

    add: nrepl + parse errors fn

--- backend-3d-scene/src/backend_3d_scene/core.clj
+++ backend-3d-scene/src/backend_3d_scene/core.clj
@@ -1,5 +1,59 @@
-(ns backend-3d-scene.core)
+(ns backend-3d-scene.core
+  (:require
+   [backend-3d-scene.nrepl :as nrepl]
+   [clojure.string :as str]
+   [mount.core :as mount])
+  (:gen-class))
+
+
+(defn- get-wrong-arity-fn-name [msg]
+  (let [[_ v] (str/split msg (re-pattern "passed to: "))]
+    (name (symbol v))))
+
+
+(defn- get-unresolved-var-name [msg]
+  (let [[_ v] (str/split msg (re-pattern "Unable to resolve symbol: | in this context"))]
+    v))
+
+
+(defn parse-error-msg [msg]
+  (condp #(str/includes? %2 %1) msg
+    "EOF while reading"
+    (str "Looks like you did not close your parenthesis.\n"
+         "Make sure that every opened form or function should be closed (my-function).\n"
+         "These are closed forms: {}, #{}, [], ()")
+
+    "Wrong number of args"
+    (str "You called " (get-wrong-arity-fn-name msg) " with a wrong number of parameters.\n"
+         "Please check your parameters.")
+
+    "Unable to resolve symbol:"
+    (str "It seems like you did not define " (get-unresolved-var-name msg) "\n"
+         "You need to define it first then you can use it.")
+
+    "cannot be cast to class clojure.lang.IFn"
+    (str "You are trying to call a function, but you have invalid code.\n"
+         "First parameter has to be always a function, following forms are invalid;\n"
+         "(12 \"Hello\") -> first parameter is a number\n"
+         "(\"Some string\") -> first parameter is a string\n"
+         "(true) -> first parameter is a boolean")
+
+    "Divide by zero" (str "Diving a number with zero is not acceptable in math :).")
+    msg))
+
+
+(comment
+  (try
+   ;(eval '(selam 1))
+   ;(read-string "(")
+   ;(/ 2 0)
+    (eval (read-string "(12 12)"))
+    (catch Exception e
+      (parse-error-msg (:cause (Throwable->map e)))
+     ;(:cause (Throwable->map e))
+      )))
 
 
 (defn -main [& args]
+  (mount/start #'nrepl/repl-server)
   (println args))
