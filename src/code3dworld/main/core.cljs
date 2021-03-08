376192d326cd93c9663a249a135cd618b807aff0
ertugrulcetin <ertu.ctn@gmail.com>
2021-03-08 11:25:17 +0300

    add: save resized window state

--- src/code3dworld/main/core.cljs
+++ src/code3dworld/main/core.cljs
@@ -8,6 +8,8 @@
 (def main-window (atom nil))
 (def backend-nrepl-port 3011)
 
+(def ls (js/require "electron-localstorage"))
+
 
 (defn- get-client []
   ((ob/get nrepl "connect") #js {:port backend-nrepl-port}))
@@ -19,12 +21,16 @@
 
 (defn init-browser []
   (reset! main-window (BrowserWindow.
-                       (clj->js {:width 1200
-                                 :height 800
+                       (clj->js {:width (max (or (.getItem ls "width") 800) 800)
+                                 :height (max (or (.getItem ls "height") 600) 600)
                                  :webPreferences {:nodeIntegration true}})))
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL (get-main-window) (str "file://" js/__dirname "/public/index.html"))
   (.on (get-main-window) "close" #(.send (.-webContents (get-main-window)) "app-close"))
+  (.on (get-main-window) "resize" (fn []
+                                    (let [[w h] (js->clj (.getSize (get-main-window)))]
+                                      (.setItem ls "width" w)
+                                      (.setItem ls "height" h))))
   (.on ipcMain "closed" #(when-not (= js/process.platform "darwin")
                            (.quit app)))
   (.on ipcMain "eval" (fn [event code]
