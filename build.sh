62a338acbe367535abf36da2f7af0d1ce8ba516a
ertugrulcetin <ertu.ctn@gmail.com>
2021-03-10 00:19:10 +0300

    add: build overwrite flag

--- build.sh
+++ build.sh
@@ -29,6 +29,7 @@ lein release
 rm -rf release-builds
 electron-packager . --prune=true \
                     --out=release-builds \
+                    --overwrite=true \
                     --ignore="(.gitignore|.clj-kondo|.github|.shadow-cljs|.idea|.lein-repl-history)"\
                     --ignore="(dev|docs|imgs|\.md|\.iml|build.sh|pom.xml|karma.conf.js|target)" \
                     --ignore="(backend-3d-scene/)" \
