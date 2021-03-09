d8c3015ca9f7dc53b6b26ac88b92da0f71c254bb
ertugrulcetin <ertu.ctn@gmail.com>
2021-03-09 13:53:13 +0300

    add: windows build job

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ cd backend-3d-scene
 lein clean
 lein uberjar
 
-cd target/uberjar
+cd target
 rm -rf classes native stale backend-3d-scene-0.1.0-SNAPSHOT.jar
-cd ../..
+cd ..
 
 echo "Make sure that you're using Oracle JDK 14!"
 # Make sure that you're using Oracle JDK 14!
@@ -14,7 +14,7 @@ echo "Make sure that you're using Oracle JDK 14!"
 JPACKAGE_PATH=/Library/Java/JavaVirtualMachines/jdk-14.0.2.jdk/Contents/Home/bin/jpackage
 $JPACKAGE_PATH --main-class backend_3d_scene.core \
                --main-jar backend-3d-scene.jar \
-               --input target/uberjar \
+               --input target \
                --name scene
 hdiutil attach scene-1.0.dmg
 cp -rf /Volumes/scene/scene.app ../resources/
