920197796a6f2d24385407ed7259279b90c9bee1
ertugrulcetin <ertu.ctn@gmail.com>
2021-03-09 12:29:27 +0300

    update: build process

--- build.sh
+++ build.sh
@@ -12,3 +12,10 @@ electron-packager . --prune=true \
                     --ignore="(dev|docs|imgs|\.md|\.iml|build.sh|pom.xml|karma.conf.js|target)" \
                     --ignore="(backend-3d-scene/)" \
                     --ignore="(project.clj|LICENSE)"
+
+if [[ $1 == "zip" ]]
+then
+  echo "zip is being generated..."
+  cd release-builds/Code\ 3D\ World-darwin-x64
+  ditto -c -k --sequesterRsrc --keepParent Code\ 3D\ World.app Code\ 3D\ World.zip
+fi
--- docs/Development.md
+++ docs/Development.md
@@ -23,3 +23,15 @@ Now, it's time to start backend (3D Scene):
 `./build.sh`
 
 **Important:** Make sure that **JPACKAGE_PATH** has Oracle JDK 14 path in `build.sh`
+
+---
+
+## Building App for Prod
+
+Make sure that you're at the root dir: `code3dworld`
+
+`./build.sh`
+
+or
+
+`./build.sh zip` (Release purpose, creates a compressed app)
