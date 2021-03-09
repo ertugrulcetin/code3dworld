48a5f85a34bfe086d7b5cc0e3e1d2a1e8c15bc94
ertugrulcetin <ertu.ctn@gmail.com>
2021-03-09 13:00:23 +0300

    update: rename build zip

--- build.sh
+++ build.sh
@@ -38,5 +38,5 @@ if [[ $1 == "zip" ]]
 then
   echo "zip is being generated..."
   cd release-builds/Code\ 3D\ World-darwin-x64
-  ditto -c -k --sequesterRsrc --keepParent Code\ 3D\ World.app Code\ 3D\ World.zip
+  ditto -c -k --sequesterRsrc --keepParent Code\ 3D\ World.app Code-3D-World-Mac-amd64.zip
 fi
